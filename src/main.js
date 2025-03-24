e2a2d162da644431e28631a961547cba02367f62
Dennis Vo <dennisphuc@gmail.com>
2025-03-24 19:18:43 +0100

    feat: finish projects section with responsive different vw size, using grid template areas

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import "../styles/components/header.css";
 import "../styles/components/hero.css";
 import "../styles/components/about.css";
 import "../styles/components/featured.css";
+import "../styles/components/work.css";
 
 import "../styles/utils.css";
